6d1a2754c45a5e9716c668a97f4376cbd63fbdaa
Lawrence <lawrence1992.pr@gmail.com>
2021-07-21 13:11:59 +0530

    changes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -6,7 +6,7 @@ function MovieCard(props) {
   return (
     <div className="movie-card">
       <div className="card">
-        <img className="card-img-top" src="props.movie.Poster" alt="" />
+        <img className="card-img-top" src={props.movie.Poster} alt="" />
         <div className="card-body">
           <h4 className="card-title">{props.movie.Title}</h4>
         </div>
